ba284a2c45636e119c46e06f6d359a70fb29bea3
Madeline Griep <mdg32@njit.edu>
2019-02-24 15:47:26 -0500

    Task: starting the project

--- public/index.php
+++ public/index.php
@@ -5,4 +5,37 @@
  * Date: 2/24/19
  * Time: 12:27 PM
  */
-echo 'This is a test';
+main::start();
+
+class main {
+
+    static public function start(){
+        $records = csv::getRecords():
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+}
+
+class csv {
+    static public function getRecords() {
+
+        $records ='test';
+        return $records;
+    }
+}
+
+class html {
+    static public function generateTable($records) {
+
+        $table =$records;
+
+        return $table;
+
+    }
+}
+
+class system {
+    static public function printPage($page) {
+        echo $page;
+    }
+}
