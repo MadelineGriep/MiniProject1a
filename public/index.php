f422a1b4a43da0d32a682b73e73ae08fe55e3108
Madeline Griep <mdg32@njit.edu>
2019-03-03 12:12:09 -0500

    Task: list all files in one array

--- public/index.php
+++ public/index.php
@@ -1,5 +1,31 @@
 <?php
-main::start("example.csv");
+
+main::start();
+
+class main
+{
+
+    static public function start(){
+
+        $file = fopen("example.csv", "r");
+
+        while(! feof($file))
+
+        {
+            $record = fgetcsv($file);
+            
+            $records[] = $record;
+        }
+
+        fclose($file);
+
+        print_r($records);
+    }
+
+}
+?>
+
+
 
 
 
